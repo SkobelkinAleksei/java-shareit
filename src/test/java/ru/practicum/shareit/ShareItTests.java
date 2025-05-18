ffeb38d7b361035b6ffc523234de573705a5677c
Алексей Игоревич <skobelkin.97@inbox.ru>
2025-05-18 21:14:58 +0300

    .

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -2,15 +2,14 @@ package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, classes = ShareItApp.class)
+@ComponentScan(basePackages = "ru.practicum.shareit")
 class ShareItTests {
 
 	@Test
 	void contextLoads() {
-		assertTrue(true);
 	}
 
 }
