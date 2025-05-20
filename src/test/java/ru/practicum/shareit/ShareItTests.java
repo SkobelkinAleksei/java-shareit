b460dea6a8619757746379e3200cf7b17f0fe670
Алексей Игоревич <skobelkin.97@inbox.ru>
2025-05-20 17:51:59 +0300

    Все также ищу ошибку почему на Git ничего не запускается

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -2,10 +2,8 @@ package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, classes = ShareItApp.class)
-@ComponentScan(basePackages = "ru.practicum.shareit")
 class ShareItTests {
 
 	@Test
